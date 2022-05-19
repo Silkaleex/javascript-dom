5a2601c3d2031e26dd972211d71753ab8f9616f4
Silkaleex <alejandropascual1995@outlook.es>
2022-05-19 16:36:19 +0200

    Javascript

---
+++ mi-primer-proyecto/elementos.js
@@ -0,0 +1,14 @@
+let nombre = "Alejandro";
+let edad = 27;
+let desarrollador = true;
+console.log(nombre, edad, desarrollador);
+const lectura = {
+  libro: "la ley del espejo",
+  autor: "Yoshori Noguchi",
+  ISBN: 978 - 84 - 17188 - 81 - 8,
+  fecha: 1 - 2 - 2019,
+  url: "https://www.amazon.es/ley-del-espejo-Yoshinori-Noguchi/dp/8417188819/ref=asc_df_8417188819/?tag=googshopes-21&linkCode=df0&hvadid=301137697361&hvpos=&hvnetw=g&hvrand=5527390702934684989&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1005509&hvtargid=pla-638488084540&psc=1",
+};
+console.log(lectura);
+const fecha_de_nacimiento = new Date("Jun 30 1995");
+console.log(fecha_de_nacimiento);
