95e2c87edf93e13881b5aae4daaedf8ac98d0d6c
Silkaleex <alejandropascual1995@outlook.es>
2022-05-21 13:35:53 +0200

    Javascript

---
+++ mi-primer-proyecto/strings/strings-cadenas.js
@@ -0,0 +1,38 @@
+//Strings & Cadenas
+let str_sng = "Comillas simples para un texto";
+let str_dbl = "Comillas dobles para un texto";
+
+console.log(str_sng);
+console.log(str_dbl);
+
+let str_comillas_simples =
+  'me dijo comento de que si podia "vete a sacar la basura"';
+let str_comillas_dobles =
+  "me dijo comento de que si podia 'vete a sacar la basura'";
+
+console.log(str_comillas_simples);
+console.log(str_comillas_dobles);
+
+//comillas invertidas(backticks)
+let str_backticks = `Estas comillas son backticks`;
+console.log(str_backticks);
+
+let nombre = "Aleex";
+let saludo = "Hola, ${nombre} Bienvenido!";
+
+console.log(saludo);
+
+//Plantillas HTML
+let plantillas = `<html>
+<h1>Hola, ${nombre} Bienvenido!</h1>
+<p>Este es un parrafo</p>
+</html>`;
+console.log(plantillas);
+//introducion de variables en html
+
+let libros = [
+  "A través del espejo",
+  "Alicia en el pais de las maravillas",
+  "Batman",
+  "Como Superarse a uno mismo",
+];
