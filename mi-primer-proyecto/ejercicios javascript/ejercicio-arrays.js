5cbe8a58408c9f3c8a6d110f2e8c3d47445917d7
Silkaleex <alejandropascual1995@outlook.es>
2022-05-29 16:55:42 +0200

    javascript

--- mi-primer-proyecto/ejercicios javascript/ejercicio-arrays.js
+++ mi-primer-proyecto/ejercicios javascript/ejercicio-arrays.js
@@ -1,27 +1,54 @@
 const lista = ["pan", "leche", "fruta", "zumo", "garbanzos"];
 lista.push("Aceite de girasol");
+lista.pop();
 console.log(lista);
 
 const eliminar = lista.filter((obj) => obj !== "Aceite de girasol");
 console.log(eliminar);
 
 const peliculasfavoritas = [
-  { titulo: "The Crow", director: "Alex Proyas", fecha: "1994" },
-  { titulo: "underwold", director: "Lakeshore Entertainment", fecha: "2003" },
-  { titulo: "Drácula", director: "Bram Stoker", fehca: "1992" },
+  {
+    titulo: "The Crow",
+    director: "Alex Proyas",
+    fecha: new Date(11, 10, 1994),
+  },
+  {
+    titulo: "underwold",
+    director: "Lakeshore Entertainment",
+    fecha: new Date(17, 10, 2003),
+  },
+  { titulo: "Drácula", director: "Bram Stoker", fecha: new Date(26, 5, 1987) },
+  {
+    titulo: "The Wolverine",
+    director: "James Mangold",
+    fecha: new Date(24, 7, 2013),
+  },
+  {
+    titulo: "Harry Potter y las reliquias de la Muerte",
+    director: "David Yates",
+    fecha: new Date(15, 7, 2011),
+  },
+  {
+    titulo: "Alicia en el país de las maravillas",
+    director: "Tim Burton",
+    fecha: new Date(25, 2, 2010),
+  },
 ];
 
 const revisionPeliculas = peliculasfavoritas.filter((obj) => obj.fecha > 2000);
 console.log(revisionPeliculas);
 
-const nuevoContenido = new Map();
-nuevoContenido.set(
-  'titulo: "The Batman", director: "Matt Reeves", fecha: "2022"'
+const peliculasActuales = peliculasfavoritas.filter(
+  (pelicula) => pelicula.fecha > new Date(2010, 0, 1)
 );
-nuevoContenido.set(
-  'titulo: "War Horse", director: "Steven Spielberg", fecha: "2012"'
-);
-console.log(nuevoContenido);
 
-const contenidoTotal = peliculasfavoritas.concat(nuevoContenido);
-console.log(contenidoTotal);
+const director = peliculasfavoritas.map((pelicula) => {
+  return pelicula.director;
+});
+
+const titulo = peliculasfavoritas.map((pelicula) => {
+  return pelicula.titulo;
+});
+
+const contenidoTotal = director.concat(titulo);
+const contenido_total = [...director, ...titulo];
