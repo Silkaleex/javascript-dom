612cec16e5e64a5f565988c921cc01178fe6196b
Silkaleex <alejandropascual1995@outlook.es>
2022-06-15 13:30:14 +0200

    Javascript

---
+++ mi-primer-proyecto/Eslint/Eslint-npm/index.js
@@ -0,0 +1,8 @@
+//Este es el que usaremos
+const nombre = "Silkaleex"
+const persona2 = "maria"
+
+/*eslint-disable */
+//En esta linea ponemos comillas simples
+//y desactives la regla las comilles dobles
+const nuevoString = "esto es un string"; //eslint-disable-line
