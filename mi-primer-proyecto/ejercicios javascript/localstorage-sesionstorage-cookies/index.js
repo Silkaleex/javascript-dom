4f43c890b8a0f7d2409a331ab9d458d1529bbd11
Silkaleex <alejandropascual1995@outlook.es>
2022-06-17 12:33:38 +0200

    Javascript

--- mi-primer-proyecto/ejercicios javascript/localstorage-sesionstorage-cookies/index.js
+++ mi-primer-proyecto/ejercicios javascript/localstorage-sesionstorage-cookies/index.js
@@ -14,3 +14,19 @@ document.cookie =
   "nombreCaducidad=Nombre;expires" +
   new Date(2022, 17, 06, 13, 00, 00).toUTCString();
 console.log(document.cookie);
+
+//Otra Manera de Hacerlo
+
+// const nombre = "Alejandro";
+// const apellido = "Pascual";
+
+// const datos = {
+//   nombre,
+//   apellido,
+// };
+
+// // sessionStorage.setItem("datos", JSON.stringify(datos))
+// // localStorage.setItem("datos", JSON.stringify(datos))
+
+// const now = new Date();
+// // document.cookie = `datos=${JSON.stringify(datos)};expires=${new Date(now.getTime() + 2 * 60000)}`
